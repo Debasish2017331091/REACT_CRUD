ab281cf1b181bf23d5474ed6883249ec22b30621
Debasish2017331091 <debasish1998khulna@gmail.com>
2023-05-31 21:38:20 +0600

    deployed website

--- src/component/MainContent/MainContent.js
+++ src/component/MainContent/MainContent.js
@@ -1,81 +1,151 @@
-import React, { useState } from 'react'
-import Button from '@mui/material/Button';
-import { Box } from '@mui/material';
-import TableData from '../Table/TableData';
+import React, { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import { Box } from "@mui/material";
+import TableData from "../Table/TableData";
 
-import {Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, TextField} from '@mui/material';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  TextField,
+} from "@mui/material";
 
-function MainContent(){
+export function createData(ticket_type, description, id) {
+  return { ticket_type, description, id };
+}
 
-    const [open, setOpen] = React.useState(false);
+function MainContent() {
+  const [counter, setCounter] = useState(7);
+  const [rows, setRows] = useState();
 
-    const [ticketType,set_ticketType] = useState("");
-    const [ticketDescription,set_ticketDescription] = useState("");
+  const [open, setOpen] = React.useState(false);
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+  const [ticketType, setTicketType] = useState("");
+  const [ticketDescription, setTicketDescription] = useState("");
 
-    const handleClose = (param) => {
-        if(param){
-            console.log(ticketType);
-            console.log(ticketDescription);
-        }
-        
-        setOpen(false);
-    };
+  useEffect(() => {
+    setRows([
+      createData("Frozen yoghurt", "Frozen yoghurt", 1),
+      createData("Frozen yoghurt", "Frozen yoghurt", 2),
+      createData("Frozen yoghurt", "Frozen yoghurt", 3),
+      createData("Frozen yoghurt", "Frozen yoghurt", 4),
+      createData("Frozen yoghurt", "Frozen yoghurt", 5),
+      createData("Frozen yoghurt", "Frozen yoghurt", 6),
+      createData("Frozen yoghurt", "Frozen yoghurt", 7),
+    ]);
+  }, []);
 
-    return(
-       <Box sx={{width:'100%', height:'100%'}}>
-            <Box sx={{display:'flex', justifyContent:'flex-end', padding:'16px'}}>
-                    <Button variant="contained" onClick={handleClickOpen} >Add Ticket Type</Button>   
-            </Box>
-            <Box>
-                    <TableData/>
-            </Box>
-            <Dialog sx={{'.MuiPaper-root':{
-                    width:'400px'
-                }}} open={open} onClose={handleClose}>
-                            <DialogTitle>Add Ticket Type</DialogTitle>
-                            <DialogContent>
-                                <TextField
-                                    autoFocus
-                                    margin="dense"
-                                    id="name"
-                                    label="Add Ticket Type"
-                                    type="Text"
-                                    fullWidth
-                                    variant="standard"
-                                    onChange={(e)=>{
-                                        console.log(e.target.value);
-                                        set_ticketType(e.target.value);
-                                    }}
-                                />
-                            </DialogContent>
-                            <p>{ticketType}</p>
-                            <DialogContent>
-                                <TextField
-                                    autoFocus
-                                    margin="dense"
-                                    id="name"
-                                    label="Add Ticket Description"
-                                    type="Text"
-                                    fullWidth
-                                    variant="standard"
-                                    onChange={(e)=>{
-                                        console.log(e.target.value);
-                                        set_ticketDescription(e.target.value);
-                                    }}
-                                />
-                            </DialogContent>
-                            <p>{ticketDescription}</p>
-                            <DialogActions>
-                            <Button onClick={handleClose}>Cancel</Button>
-                            <Button onClick={handleClose}>Add Ticket Type</Button>
-                            </DialogActions>
-            </Dialog>
-       </Box>
-    )
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setTicketType("");
+    setTicketDescription("");
+    setOpen(false);
+  };
+
+  const handleAddTicket = () => {
+    if (ticketType.trim().length > 0) {
+      const ticketData = createData(
+        ticketType.trim(),
+        ticketDescription.trim(),
+        counter + 1
+      );
+      setCounter((prev) => prev + 1);
+      setRows((prev) => [...prev, ticketData]);
+      handleClose();
+    } else {
+      alert("ticket type field is required!");
+    }
+  };
+
+  const handleDelete = (id) => {
+    console.log(id);
+    setRows((prevRows) => {
+      return rows.filter((item) => item.id !== id);
+    });
+  };
+
+  const handleUpdate = (updatedData) => {
+    setRows((rows) => {
+      const prevRows = [...rows];
+      const index = prevRows.findIndex((row) => row.id === updatedData.id);
+      console.log({ index });
+      if (index !== -1) {
+        prevRows[index] = updatedData;
+      }
+      return prevRows;
+    });
+  };
+
+  return (
+    <Box sx={{ width: "100%", height: "100%" }}>
+      <Box
+        sx={{ display: "flex", justifyContent: "flex-end", padding: "16px" }}
+      >
+        <Button variant="contained" onClick={handleClickOpen}>
+          Add Ticket Type
+        </Button>
+      </Box>
+      <Box>
+        <TableData
+          data={rows}
+          onDelete={handleDelete}
+          onUpdate={handleUpdate}
+        />
+      </Box>
+      <Dialog
+        sx={{
+          ".MuiPaper-root": {
+            width: "400px",
+          },
+        }}
+        open={open}
+        onClose={handleClose}
+      >
+        <DialogTitle>Add Ticket Type</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Add Ticket Type"
+            type="Text"
+            fullWidth
+            variant="standard"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setTicketType(e.target.value);
+            }}
+          />
+        </DialogContent>
+        <p>{ticketType}</p>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Add Ticket Description"
+            type="Text"
+            fullWidth
+            variant="standard"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setTicketDescription(e.target.value);
+            }}
+          />
+        </DialogContent>
+        <p>{ticketDescription}</p>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleAddTicket}>Add Ticket Type</Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
+  );
 }
 
 export default MainContent;
--- src/component/Table/TableData.js
+++ src/component/Table/TableData.js
@@ -1,142 +1,185 @@
-import React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import React, { useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
-import { Button } from '@mui/material';
+import { Button } from "@mui/material";
 
 // Following Two are for edit and delete icons
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 // Following code is for dialog box
-import {Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, TextField} from '@mui/material';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  TextField,
+} from "@mui/material";
+import { createData } from "../MainContent/MainContent";
 
+// var rows = [
+//   createData('Frozen yoghurt', 'Frozen yoghurt', 1),
+//   createData('Frozen yoghurt', 'Frozen yoghurt', 2),
+//   createData('Frozen yoghurt', 'Frozen yoghurt', 3),
+//   createData('Frozen yoghurt', 'Frozen yoghurt', 4),
+//   createData('Frozen yoghurt', 'Frozen yoghurt', 5),
+//   createData('Frozen yoghurt', 'Frozen yoghurt', 6),
+//   createData('Frozen yoghurt', 'Frozen yoghurt', 7)
+// ];
+// var cnt=7;
 
-function createData(
-  ticket_type,
-  description,
-  id
-) {
-  return { ticket_type, description, id};
-}
-
-var rows = [
-  createData('Frozen yoghurt', 'Frozen yoghurt', 1),
-  createData('Frozen yoghurt', 'Frozen yoghurt', 2),
-  createData('Frozen yoghurt', 'Frozen yoghurt', 3),
-  createData('Frozen yoghurt', 'Frozen yoghurt', 4),
-  createData('Frozen yoghurt', 'Frozen yoghurt', 5),
-  createData('Frozen yoghurt', 'Frozen yoghurt', 6),
-  createData('Frozen yoghurt', 'Frozen yoghurt', 7)
-];
-var cnt=7;
-
-export default function TableData() {
-  
+export default function TableData(props) {
   const [openEditPage, setOpenEditPage] = React.useState(false);
+  const [openedId, setOpenedId] = useState("");
+  const [editTicketType, setEditTicketType] = useState("");
+  const [editTicketDescription, setEditTicketDescription] = useState("");
 
-  const handleClickOpenEditPage = () => {
-      setOpenEditPage(true);
+  const handleClickOpenEditPage = (id) => {
+    setOpenedId(id);
+    const selectedRow = props?.data?.find((row) => row.id === id);
+    setEditTicketType(selectedRow?.ticket_type);
+    setEditTicketDescription(selectedRow?.description);
+    setOpenEditPage(true);
   };
 
   const handleCloseEditPage = () => {
-      setOpenEditPage(false);
+    setOpenEditPage(false);
+  };
+
+  const handleUpdate = () => {
+    if(editTicketType.trim().length > 0){
+      const updatedRow = createData(
+        editTicketType.trim(),
+        editTicketDescription.trim(),
+        openedId
+      );
+      props.onUpdate(updatedRow);
+      handleCloseEditPage();
+    }else{
+      alert('empty ticket type')
+    }
+    
   };
 
   const [openDeletePage, setOpenDeletePage] = React.useState(false);
 
-  const handleClickOpenDeletePage = () => {
-      setOpenDeletePage(true);
+  const handleClickOpenDeletePage = (id) => {
+    setOpenedId(id);
+    setOpenDeletePage(true);
   };
 
   const handleCloseDeletePage = () => {
-      setOpenDeletePage(false);
+    setOpenDeletePage(false);
   };
-  
+
+  const handleDeleteRow = () => {
+    props.onDelete(openedId);
+    handleCloseDeletePage();
+  };
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead sx={{backgroundColor: '#f0f0f0'}}>
+        <TableHead sx={{ backgroundColor: "#f0f0f0" }}>
           <TableRow>
-            <TableCell align="center" sx={{ color: 'blue' }}>Ticket Name</TableCell>
-            <TableCell align="center" sx={{ color: 'blue' }}>Description</TableCell>
-            <TableCell align="center" sx={{ color: 'blue' }}>Action</TableCell>
+            <TableCell align="center" sx={{ color: "blue" }}>
+              Ticket Name
+            </TableCell>
+            <TableCell align="center" sx={{ color: "blue" }}>
+              Description
+            </TableCell>
+            <TableCell align="center" sx={{ color: "blue" }}>
+              Action
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {props?.data?.map((row) => (
             <TableRow
               key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell align="center">{row.ticket_type}</TableCell>
               <TableCell align="center">{row.description}</TableCell>
               <TableCell align="center">
-                  <Button onClick={handleClickOpenEditPage} >
-                      <EditIcon/>
-                  </Button>
-                  
-                  <Button onClick={handleClickOpenDeletePage}>
-                      <DeleteForeverIcon/>
-                  </Button>
+                <Button onClick={() => handleClickOpenEditPage(row.id)}>
+                  <EditIcon />
+                </Button>
+
+                <Button onClick={() => handleClickOpenDeletePage(row.id)}>
+                  <DeleteForeverIcon />
+                </Button>
               </TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
-            <Dialog sx={{'.MuiPaper-root':{
-                    width:'400px'
-                }}} open={openEditPage} onClose={handleCloseEditPage}>
-                            <DialogTitle>Edit Ticket Type</DialogTitle>
-                            <DialogContent>
-                                <TextField
-                                    autoFocus
-                                    margin="dense"
-                                    id="name"
-                                    label="Ticket Type"
-                                    type="Text"
-                                    fullWidth
-                                    variant="standard"
-                                />
-                            </DialogContent>
-                            <p></p>
-                            <DialogContent>
-                                <TextField
-                                    autoFocus
-                                    margin="dense"
-                                    id="name"
-                                    label="Add Ticket Description"
-                                    type="Text"
-                                    fullWidth
-                                    variant="standard"
-                                />
-                            </DialogContent>
-                            <p></p>
-                            <DialogActions>
-                            <Button onClick={handleCloseEditPage}>Cancel</Button>
-                            <Button onClick={handleCloseEditPage}>Save Changes</Button>
-                            </DialogActions>
-            </Dialog>
-            <Dialog sx={{'.MuiPaper-root':{
-                    width:'400px'
-                }}} open={openDeletePage} onClose={handleClickOpenDeletePage}>
-                            <DialogTitle>Delete</DialogTitle>
-                            <DialogContent>
-                              <DialogContentText>
-                                Are you Sure to delete this?
-                              </DialogContentText>
-                            </DialogContent>
-                            
-                            <DialogActions>
-                            <Button onClick={handleCloseDeletePage}>No</Button>
-                            <Button onClick={handleCloseDeletePage}>Yes</Button>
-                            </DialogActions>
-            </Dialog>
+      <Dialog
+        sx={{
+          ".MuiPaper-root": {
+            width: "400px",
+          },
+        }}
+        open={openEditPage}
+        onClose={handleCloseEditPage}
+      >
+        <DialogTitle>Edit Ticket Type</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Ticket Type"
+            type="Text"
+            fullWidth
+            variant="standard"
+            value={editTicketType}
+            onChange={(e) => setEditTicketType(e.target.value)}
+          />
+        </DialogContent>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Add Ticket Description"
+            type="Text"
+            fullWidth
+            variant="standard"
+            value={editTicketDescription}
+            onChange={(e) => setEditTicketDescription(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseEditPage}>Cancel</Button>
+          <Button onClick={handleUpdate}>Save Changes</Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog
+        sx={{
+          ".MuiPaper-root": {
+            width: "400px",
+          },
+        }}
+        open={openDeletePage}
+        onClose={handleCloseDeletePage}
+      >
+        <DialogTitle>Delete</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Are you Sure to delete this?</DialogContentText>
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={handleCloseDeletePage}>No</Button>
+          <Button onClick={handleDeleteRow}>Yes</Button>
+        </DialogActions>
+      </Dialog>
     </TableContainer>
   );
 }
